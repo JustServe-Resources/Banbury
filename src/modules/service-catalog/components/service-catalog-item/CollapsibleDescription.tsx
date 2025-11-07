8b3eecbced8b537137307c61f8376ca0253fcef8
DevSciCloan <116118420+DevSciCloan@users.noreply.github.com>
2025-11-07 15:10:44 -0700

    Adding Approval Requests

--- src/modules/service-catalog/components/service-catalog-item/CollapsibleDescription.tsx
+++ src/modules/service-catalog/components/service-catalog-item/CollapsibleDescription.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
 import { Button } from "@zendeskgarden/react-buttons";
 import ChevronUp from "@zendeskgarden/svg-icons/src/16/chevron-up-fill.svg";
@@ -6,34 +6,38 @@ import ChevronDown from "@zendeskgarden/svg-icons/src/16/chevron-down-fill.svg";
 import { useTranslation } from "react-i18next";
 import { getColorV8 } from "@zendeskgarden/react-theming";
 import { XXXL } from "@zendeskgarden/react-typography";
+import { ItemThumbnail } from "../item-thumbnail/ItemThumbnail";
 
 const DescriptionWrapper = styled.div`
   border-bottom: ${(props) => props.theme.borders.sm}
     ${(props) => getColorV8("grey", 300, props.theme)};
   padding-bottom: ${(props) => props.theme.space.lg};
-  margin-right: ${(props) => props.theme.space.xl};
+  margin-inline-end: ${(props) => props.theme.space.xl};
 
   @media (max-width: ${(props) => props.theme.breakpoints.md}) {
-    margin-right: 0;
+    margin-inline-end: 0;
   }
 `;
 
 const ItemTitle = styled(XXXL)`
   font-weight: ${(props) => props.theme.fontWeights.semibold};
+  margin-bottom: 0;
+  overflow-wrap: break-word;
+  max-width: 100%;
 `;
 
-const CollapsibleText = styled.div<{ expanded: boolean }>`
+const CollapsibleText = styled.div<{ isCollapsed: boolean }>`
   font-size: ${(props) => props.theme.fontSizes.md};
-  text-align: left;
   display: -webkit-box;
   -webkit-box-orient: vertical;
-  -webkit-line-clamp: ${(props) => (props.expanded ? "none" : 3)};
+  -webkit-line-clamp: ${(props) => (props.isCollapsed ? 3 : "none")};
   overflow: hidden;
   margin-top: ${(props) => props.theme.space.md};
-  padding-right: ${(props) => props.theme.space.xl};
+  padding-inline-end: ${(props) => props.theme.space.xl};
+  overflow-wrap: break-word;
 
   @media (max-width: ${(props) => props.theme.breakpoints.md}) {
-    padding-right: 0;
+    padding-inline-end: 0;
   }
 `;
 
@@ -45,39 +49,70 @@ const ToggleButton = styled(Button)`
   }
 `;
 
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${(props) => props.theme.space.md};
+`;
+
 interface CollapsibleDescriptionProps {
   title: string;
   description: string;
+  thumbnailUrl: string;
 }
-
-const DESCRIPTION_LENGTH_THRESHOLD = 270;
 
 export const CollapsibleDescription = ({
   title,
   description,
+  thumbnailUrl,
 }: CollapsibleDescriptionProps) => {
-  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
+  const [isClamped, setIsClamped] = useState<boolean>(false);
   const { t } = useTranslation();
+  const contentRef = useRef<HTMLDivElement>(null);
 
-  const showToggleButton = description.length > DESCRIPTION_LENGTH_THRESHOLD;
+  useEffect(() => {
+    const el = contentRef.current;
+    if (el) {
+      const checkClamped = () => {
+        const visibleBoxHeight = el.getBoundingClientRect().height;
+        const fullContentHeight = el.scrollHeight;
+        const clamped = fullContentHeight - visibleBoxHeight > 1;
+        setIsClamped(clamped);
+        if (!clamped) {
+          setIsCollapsed(false);
+        }
+      };
+      requestAnimationFrame(checkClamped);
+    }
+  }, [description]);
 
   const toggleDescription = () => {
-    setIsExpanded(!isExpanded);
+    setIsCollapsed(!isCollapsed);
   };
 
   return (
     <DescriptionWrapper>
-      <ItemTitle tag="h1">{title}</ItemTitle>
-      <CollapsibleText expanded={isExpanded || !showToggleButton}>
-        {description}
-      </CollapsibleText>
-      {showToggleButton && (
+      <HeaderContainer>
+        <ItemThumbnail size="large" url={thumbnailUrl} />
+        <ItemTitle tag="h1">{title}</ItemTitle>
+      </HeaderContainer>
+      {description && (
+        <CollapsibleText
+          ref={contentRef}
+          className="service-catalog-description"
+          isCollapsed={isCollapsed}
+          dangerouslySetInnerHTML={{ __html: description }}
+        ></CollapsibleText>
+      )}
+      {isClamped && (
         <ToggleButton isLink onClick={toggleDescription}>
-          {isExpanded
+          {!isCollapsed
             ? t("service-catalog.item.read-less", "Read less")
             : t("service-catalog.item.read-more", "Read more")}
           <Button.EndIcon>
-            {isExpanded ? <ChevronUp /> : <ChevronDown />}
+            {!isCollapsed ? <ChevronUp /> : <ChevronDown />}
           </Button.EndIcon>
         </ToggleButton>
       )}
